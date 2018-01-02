98a56da93eb1bc5287dfc24ec37cddbb98561d7a
Eshan Mewantha Herath <eshanMewantha@users.noreply.github.com>
2018-01-02 23:04:44 +0530

    first draft of pure python clustering algorithm

---
+++ k-means-clustering/pure-python/main.py
@@ -0,0 +1,37 @@
+## To Be Changed
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+    'x': [12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72],
+    'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
+})
+
+
+np.random.seed(200)
+k = 3
+
+centroids = {
+    i+1: [np.random.randint(0, 80), np.random.randint(0, 80)]
+    for i in range(k)
+}
+
+
+def assignment(df, centroids):
+    for i in centroids.keys():
+        df['distance_from_{}'.format(i)] = (
+            np.sqrt(
+                (df['x'] - centroids[i][0]) ** 2
+                + (df['y'] - centroids[i][1]) ** 2
+            )
+        )
+    centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
+    df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
+    df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
+    # df['color'] = df['closest'].map(lambda x: colmap[x])
+    return df
+
+df = assignment(df, centroids)
+print(df.head())
+
